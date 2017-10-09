6421bb74d41f82f291174ab251506dd3a0a1f5b6
sjetley <saumya.jetley@ox.ac.uk>
2017-10-09 17:33:19 +0100

    2 softmax operations in succession, see how this works

--- VGGNet/models/models_CIFARS/2_level_atten_1global_pred-ensemble/vgg_match.lua
+++ VGGNet/models/models_CIFARS/2_level_atten_1global_pred-ensemble/vgg_match.lua
@@ -11,16 +11,18 @@ local context_vec1_bn = nn.BatchNormalization(512)(context_vec1)
 local context_vec1_relu = nn.ReLU(true)(context_vec1_bn)
 local context_vec1_do = nn.Dropout(0.5)(context_vec1_relu)
 local bin_class1 = nn.Linear(512, cmd_params.num_classes)(context_vec1_do)
+local class_prob1 = nn.Softmax(bin_class1)
 
 local context_vec2_bn = nn.BatchNormalization(512)(context_vec2)
 local context_vec2_relu = nn.ReLU(true)(context_vec2_bn)
 local context_vec2_do = nn.Dropout(0.5)(context_vec2_relu)
 local bin_class2 = nn.Linear(512, cmd_params.num_classes)(context_vec2_do)
+local class_prob2 = nn.SoftMax(bin_class2)
 
-local bin_class = nn.CAddTable()({bin_class1,bin_class2})
+local class_prob = nn.CAddTable()({class_prob1,class_prob2})
 
 local output = {}
-table.insert(output, bin_class)
+table.insert(output, class_prob)
 
 local match = nn.gModule(input, output)
 
